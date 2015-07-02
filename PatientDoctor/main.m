088ab842a04cab46b2d572b177207fc4afe17368
WaterBleu <jeff7746@hotmail.com>
2015-07-02 10:27:25 -0700

    Initial Commit

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by Jeff Huang on 2015-07-02.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
